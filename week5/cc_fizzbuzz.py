9f27dfb1e9e0765b7edee55d8fabe61df2275bed
JuanBurgos <JuanBurgos@outlook.com>
2021-09-10 13:31:08 -0400

    FizzBuzz Code Challenge

---
+++ week5/cc_fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(num):
+    for num in range(1, num+1):
+
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+fizzbuzz(50)
